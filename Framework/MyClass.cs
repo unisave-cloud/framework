e5d37aa8f806c7a4ae862ecb87438bacf5e74cb1
Jirka-Mayer <jirkamayer97@gmail.com>
2019-04-24 11:39:17 +0200

    Initial commit

---
+++ Framework/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unisave.Framework
+{
+    public class MyClass
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Unisave framework says hello!");
+        }
+    }
+}
