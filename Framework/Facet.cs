1f4e9aa3a6c4f9801256503b2e6c8eafeec9c544
Jirka-Mayer <jirkamayer97@gmail.com>
2019-07-11 18:19:06 +0200

    Facet definition

---
+++ Framework/Facet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace Unisave
+{
+    public abstract class Facet
+    {
+        /// <summary>
+        /// The player who called this facet
+        /// </summary>
+        protected UnisavePlayer Caller { get; private set; }
+
+        public static Facet CreateInstance(Type facetType, UnisavePlayer caller)
+        {
+            if (!typeof(Facet).IsAssignableFrom(facetType))
+                throw new ArgumentException(
+                    "Provided type does not inherit from the Facet class.",
+                    nameof(facetType)
+                );
+
+            ConstructorInfo ci = facetType.GetConstructor(new Type[] { });
+
+            if (ci == null)
+            {
+                throw new ArgumentException(
+                    "Provided facet type " + facetType + " lacks empty constructor.",
+                    nameof(facetType)
+                );
+            }
+
+            Facet facet = (Facet)ci.Invoke(new object[] { });
+
+            facet.Caller = caller;
+
+            return facet;
+        }
+    }
+}
