4b6d39f728af02ada4ae4c38424838958140f6da
Jirka-Mayer <jirkamayer97@gmail.com>
2019-07-19 09:28:32 +0200

    Load entity by id

--- UnisaveFramework/Facades/GetEntity.cs
+++ UnisaveFramework/Facades/GetEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Unisave.Database;
+using Unisave.Runtime;
 
 namespace Unisave
 {
@@ -14,6 +15,17 @@ namespace Unisave
 
         public static E First() // for entities belonging to the game
             => QueryBuilder<E>.Create().First();
+
+        /// <summary>
+        /// Obtain an entity by it's ID
+        /// </summary>
+        public static E Find(string entityId)
+        {
+            var raw = Endpoints.Database.LoadEntity(entityId);
+            var entity = Entity.CreateInstance(typeof(E));
+            Entity.LoadRawEntity(raw, entity);
+            return (E)entity;
+        }
 
         public static QueryBuilder<E> OfPlayer(UnisavePlayer player)
             => QueryBuilder<E>.Create().OfPlayer(player);
