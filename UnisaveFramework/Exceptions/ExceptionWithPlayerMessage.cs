46c847f6db3942e06a78e18093ebe0d39d768ad2
Jirka-Mayer <jirkamayer97@gmail.com>
2019-08-23 16:29:12 +0200

    Fixed exception with player message serialization

--- UnisaveFramework/Exceptions/ExceptionWithPlayerMessage.cs
+++ UnisaveFramework/Exceptions/ExceptionWithPlayerMessage.cs
@@ -20,7 +20,22 @@ namespace Unisave.Exceptions
         public ExceptionWithPlayerMessage(string message, System.Exception inner) : base(message, inner) { }
         protected ExceptionWithPlayerMessage(
             System.Runtime.Serialization.SerializationInfo info,
-            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+            System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+            MessageForPlayer = info.GetString(nameof(MessageForPlayer));
+        }
+
+        public override void GetObjectData(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context
+        )
+        {
+            if (info == null)
+                throw new ArgumentNullException("info");
+
+            info.AddValue(nameof(MessageForPlayer), MessageForPlayer);
+            base.GetObjectData(info, context);
+        }
 
         /// <summary>
         /// Chainable method for setting player message
