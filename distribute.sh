24952456fb2b20c4c098e8a4e221d0c9cffb11a6
Jirka-Mayer <jirkamayer97@gmail.com>
2019-08-27 13:38:23 +0200

    Modified distribution to match new web framework location

--- distribute.sh
+++ distribute.sh
@@ -3,19 +3,12 @@
 # 
 # This file thus works only on my PC and not anywhere else.
 
-##################
-# Patch PDB file #
-##################
-
-# DISABLED
-#mono ~/ImportantCode/SharpPdbPatcher/Bin/Debug/SharpPdbPatcher.exe --regex ".*UnisaveFramework" --replace "UF:" ./UnisaveFramework/bin/Debug/UnisaveFramework.dll
-
 ##############
 # Distribute #
 ##############
 
 # web
-cp -R UnisaveFramework/bin/Debug/* ~/ImportantCode/Unisave/Web/resources/assemblies/framework/0.2.0
+cp -R UnisaveFramework/bin/Debug/* ~/ImportantCode/Unisave/Web/storage/app/unisave-framework/0.2.0
 
 # database proxy
 cp -R UnisaveFramework/bin/Debug/* ~/ImportantCode/Unisave/DatabaseProxy/dlls/UnisaveFramework
