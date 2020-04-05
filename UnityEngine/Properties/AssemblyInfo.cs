f9538bd861e5b85a2d8bc7e6ea628beefa167aba
Jirka-Mayer <jirkamayer97@gmail.com>
2020-04-05 02:10:21 +0200

    Renamed FakeUnity to UnityEngine so that the framework compiles inside Unity editor

---
+++ UnityEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("UnityEngine")]
+[assembly: AssemblyDescription(
+    "Fake UnityEngine.dll implementing ony things " +
+    "that might beneeded on the server"
+)]
+[assembly: AssemblyVersion("0.0.0.0")]
