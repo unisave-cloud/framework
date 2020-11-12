dc3a2a0a3884f8aaec5442b7a555ca2476bd9576
Jirka-Mayer <jirkamayer97@gmail.com>
2020-11-13 00:08:42 +0100

    Cleaned up the Broadcast facade

--- UnisaveFramework/Facades/Broadcast.cs
+++ UnisaveFramework/Facades/Broadcast.cs
@@ -8,16 +8,6 @@ namespace Unisave.Facades
     /// </summary>
     public static class Broadcast
     {
-        private static IBroadcaster GetBroadcaster()
-        {
-            if (!Facade.HasApp)
-                throw new InvalidOperationException(
-                    "You cannot broadcast messages from the client side."
-                );
-            
-            return Facade.App.Resolve<IBroadcaster>();
-        }
-        
         /// <summary>
         /// Access broadcasting on a channel
         /// </summary>
@@ -27,34 +17,6 @@ namespace Unisave.Facades
             where TChannel : BroadcastingChannel, new()
         {
             return new TChannel();
-            
-            //BroadcastingChannel.ValidateParameters<TChannel>(parameters);
-            //return new ChannelHandle<TChannel>(parameters);
-        }
-
-        /// <summary>
-        /// Helper class to allow for method chaining
-        /// </summary>
-        public class ChannelHandle<TChannel>
-            where TChannel : BroadcastingChannel, new()
-        {
-            private readonly string[] parameters;
-            
-            public ChannelHandle(string[] parameters)
-            {
-                this.parameters = parameters;
-            }
-            
-            /// <summary>
-            /// Sends a message to the channel
-            /// </summary>
-            /// <param name="message">The message to be sent</param>
-            public void Send(BroadcastingMessage message)
-            {
-                Broadcast
-                    .GetBroadcaster()
-                    .BroadcastMessage<TChannel>(parameters, message);
-            }
         }
     }
 }
